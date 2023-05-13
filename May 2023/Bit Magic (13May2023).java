e7908f159424e9cc315df2ecd693f4934b2c9c06
Satyam Raj <SSTYMRJ@GMAIL.COM>
2023-05-13 08:47:15 +0530

    Update Bit Magic (13May2023).java

--- May 2023/Bit Magic (13May2023).java
+++ May 2023/Bit Magic (13May2023).java
@@ -62,3 +62,36 @@ Constraints:
 
 
 
+class Solution {
+
+    public static int bitMagic(int n, int[] arr) {
+
+        int i=0;
+
+        int j=n-1;
+
+        int totalMismatch=0;
+
+        while(i<=j){
+
+            if(arr[i]!=arr[j])
+
+                totalMismatch++;
+
+            i++;
+
+            j--;
+
+        }
+
+        if(totalMismatch==0)
+
+            return 0;
+
+        else 
+
+            return (totalMismatch%2==0) ? (totalMismatch/2) : ((totalMismatch/2)+1);
+
+    }
+
+}
